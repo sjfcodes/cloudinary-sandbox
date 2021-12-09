1ee9c61465b87c2499eedac46efa9b245fd7b9ad
Samuel Fox <samueljasonfox@gmail.com>
2021-12-09 04:07:58 -0800

    add code to remove file form server after upload

--- controller/uploadRoutes.js
+++ controller/uploadRoutes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
+const { unlink } = require('fs')
 const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
 require('dotenv').config()
 
-const upload = multer({ dest: './public/data/uploads' });
+const upload = multer({ dest: './_temp' });
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -12,27 +13,24 @@ cloudinary.config({
     secure: true
 });
 
-router.post('/upload', upload.single('myFile'), (req, res) => {
-    console.log(req.body); // req.body will hold the text fields appended to your formData object
-    console.log(req.file); // req.file is the name of your file in the form above, here 'uploaded_file'
-
+router.post('/upload', upload.single('myFile'), async (req, res) => {
+    // console.log(req.body); // req.body will hold the text fields appended to your formData object
+    // console.log(req.file); // req.file is the name of your file in the form above, here 'uploaded_file'
     const { file: { filename, destination } } = req;
     const pathToFile = `${destination}/${filename}`;
-
-    cloudinary.uploader.upload(pathToFile,
-        {
-            resource_type: "image",
-            public_id: 'cloudinary-sandbox-demo', // folder name to store in cloudinary 
-        }, (error, result) => console.log(result, error)
-    )
-        .then(data => {
-            console.log(data);
-            res.json(data.url);
-        })
-        .catch(error => {
-            console.error(error);
-            res.json({ msg: 'failure' })
-        });
+    try {
+        const { url } = await cloudinary.uploader.upload(pathToFile,
+            {
+                resource_type: "image",
+                public_id: 'cloudinary-sandbox-demo', // folder name to store in cloudinary 
+            }, (error) => console.error(error)
+        )
+        res.json(url);
+        unlink(pathToFile, (error) => console.error(error))
+    } catch (error) {
+        console.error(error);
+        res.json({ msg: 'failure' })
+    }
 });
 
 module.exports = router;
