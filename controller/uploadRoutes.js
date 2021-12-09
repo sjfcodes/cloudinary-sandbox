bf7a4f9ad2eb25ab97b08a7d84c0929fae736f45
Samuel Fox <samueljasonfox@gmail.com>
2021-12-09 03:34:01 -0800

    linked to cloudinary

--- controller/uploadRoutes.js
+++ controller/uploadRoutes.js
@@ -1,53 +1,46 @@
 const router = require('express').Router();
+const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
-// https://www.npmjs.com/package/multer
-
+require('dotenv').config()
 
 const upload = multer({ dest: './public/data/uploads' });
-/**
- * simple file upload
- * 
- * * * * * * * *
- *    myFile   * ---> key used to extract file from FormData object set from the client
- * * * * * * * *
- */
-router.post('/upload', upload.single('myFile'), function (req, res) {
 
-    // req.body will hold the text fields, if there were any 
-    console.log(req.body);
-    // req.file is the name of your file in the form above, here 'uploaded_file'
-    console.log(req.file);
-
-    res.json({ msg: 'success' })
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+    secure: true
 });
 
+router.post('/upload', upload.single('myFile')
+    , function (req, res) {
+
+        // req.body will hold the text fields, if there were any 
+        console.log(req.body);
+        // req.file is the name of your file in the form above, here 'uploaded_file'
+        console.log(req.file);
+        const { file: { filename, destination } } = req
+
+        const pathToFile = `${destination}/${filename}`
+        cloudinary.uploader.upload(pathToFile,
+            {
+                resource_type: "image",
+                public_id: 'cloudinary-sandbox-demo',
+            },
+            (error, result) => { console.log(result, error) }
+        )
+            .then(data => {
+                console.log(data);
+                res.json(data.url);
+            })
+            .catch(error => {
+                console.error(count, error);
+                res.json({ msg: 'failure' })
+            });
 
 
-// /**
-//  * optional - setup multer disk Storage to control file name
-//  */
-// var { extname } = require('path');
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, './public/data/uploads');
-//     },
-//     filename: function (req, file, cb) {
-//         const fileExt = file.mimetype.match(/jpg|jpeg|png|gif/i);
-//         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-//         cb(null, `${file.fieldname}-${uniqueSuffix}.${fileExt}`);
-//     }
-// });
-// const upload = multer({ storage });
-
-// router.post('/upload', upload.single('myFile'), function (req, res) {
-
-//     // req.body will hold the text fields, if there were any 
-//     console.log(req.body);
-//     // req.file is the name of your file in the form above, here 'uploaded_file'
-//     console.log(req.file);
-
-//     res.json({ msg: 'success' });
-// });
+    }
+);
 
 
 module.exports = router;
