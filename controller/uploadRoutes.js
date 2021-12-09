21a82dc2507a46c588f2e502b9d1fe5fdfea5fc3
Samuel Fox <samueljasonfox@gmail.com>
2021-12-09 12:05:08 -0800

    remove server side console.logs

--- controller/uploadRoutes.js
+++ controller/uploadRoutes.js
@@ -14,8 +14,8 @@ cloudinary.config({
 });
 
 router.post('/upload', upload.single('myFile'), async ({ body, file }, res) => {
-    console.log(req.body); // req.body will hold the text fields appended to your formData object
-    console.log(req.file); // req.file is the name of your file in the form above, here 'uploaded_file'
+    // console.log(req.body); // req.body will hold the text fields appended to your formData object
+    // console.log(req.file); // req.file is the name of your file in the form above, here 'uploaded_file'
     const { filename, destination } = file;
     const pathToFile = `${destination}/${filename}`;
     try {
