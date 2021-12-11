412152150cbb6cc87c0d3339b26929e0f233c983
Samuel Fox <samueljasonfox@gmail.com>
2021-12-10 18:25:56 -0800

    setup multimedia uploads

--- controller/uploadRoutes.js
+++ controller/uploadRoutes.js
@@ -4,7 +4,7 @@ const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
 require('dotenv').config();
 
-const upload = multer({ dest: './utils/_temp-image-store' });
+const upload = multer({ dest: './utils/_temp-file-store' });
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -13,31 +13,40 @@ cloudinary.config({
     secure: true
 });
 
-router.post('/upload', upload.single('myFile'), async ({ body, file }, res) => {
-    // console.log(req.body); // req.body will hold the text fields appended to your formData object
-    // console.log(req.file); // req.file is the name of your file in the form above, here 'uploaded_file'
-    const { filename, destination } = file;
-    const pathToFile = `${destination}/${filename}`;
+const auth = (req, res, next) => {
+    if (req.headers.authorization.substring(7) === process.env.SERVER_PW) return next()
+    res.status(401).json({ message: 'unauthorized' })
+}
+
+router.post('/upload', auth, upload.single('myFile'), async ({ body, file }, res) => {
     try {
-        const { url } = await cloudinary.uploader
+        // console.log(body); // body will hold the text fields appended to your formData object
+        // console.log(file); // file is the name of your file in the form above, here 'uploaded_file'
+        console.log(file)
+        const { filename, destination, mimetype } = file;
+        const pathToFile = `${destination}/${filename}`;
+        // https://cloudinary.com/documentation/image_upload_api_reference#upload_optional_parameters
+        const data = await cloudinary.uploader
             .upload(pathToFile, {
-                resource_type: "image",
+                // mime type starts with 'image' || 'video'|| 'auido'
+                resource_type: mimetype.split('/')[0] === 'image' ? 'image' : 'video',
                 public_id: 'cloudinary-sandbox-demo', // folder name to store in cloudinary 
             },
                 (error) => {
                     if (error) console.error(error)
                 }
             );
+        console.log(data)
 
-        const httpsUrl = `https://${url.split('://')[1]}` // convert http to https
+        const httpsUrl = `https://${data.url.split('://')[1]}` // convert http to https
 
         /**
          * the resourse is now hosted, send back the url to user.
          * if you'd like to save the url to a db, do this here
          */
 
-        res.json({ ...body, url: httpsUrl }); // respond with url and  inital request body
-        // delete file from utils/_temp-image-store folder 
+        res.json({ ...body, url: httpsUrl, mimetype }); // respond with url and  inital request body
+        // delete file from utils/_temp-file-store folder 
         unlink(pathToFile, (error) => {
             if (error) console.error(error)
         });
--- public/js/script.js
+++ public/js/script.js
@@ -1,64 +1,123 @@
 window.addEventListener('load', function () {
-    const img = document.getElementById('img-preview');
-    const fileInputEl = document.querySelector('input[name="my-file"]')
-    const descriptionInputEl = document.querySelector('input[name="my-description"]')
-    const submitBtn = document.querySelector('input[type=submit]')
-    const hostedLinkEl = document.getElementById('hosted-link')
-    let myFile
+    const formEl = document.querySelector('form')
+    const fileInputEl = document.querySelector('input[name="my-file"]');
+    const fileInputLabel = document.getElementById('file-name');
+    const descriptionInputEl = document.querySelector('textarea[name="my-description"]');
+    const passwordInputEl = document.querySelector('input[type=password')
+    const submitBtn = document.querySelector('button[type=submit]');
+    const hostedLinkEl = document.getElementById('hosted-link');
+    /** * * * * * * * * * * * * * * * * * * * * * * * * * * *
+     * when a media file is uploaded, the type (or mimetype)
+     * of the file will have the value
+     * '<type>/<file-extention>'
+     * 
+     * ex: 
+     *      'image/png', 'audio/mp3', 'video/mp4'
+     * ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
+     * using the keys 'audio', 'image', 'video'
+     * by grabbing the mimetype text before the slash
+     * the correct DOM node can be targeted.
+     * ex:
+     *      mediaType['image'] (target the image element)
+     * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+    const mediaType = {
+        audio: document.getElementById('audio-preview'),
+        image: document.getElementById('image-preview'),
+        video: document.getElementById('video-preview')
+    };
 
-    const resetPage = () => {
-        fileInputEl.value = ''
-        descriptionInputEl.value = ''; // reset displayed text 
-        submitBtn.setAttribute('disabled', true) // disable submit btn
-        img.setAttribute('src', '') // remove selected image
-        hostedLinkEl.innerText = '' // reset link text
-    }
+    let myFile;
 
-    const displayResults = (url) => {
-        img.setAttribute('src', url)
-        hostedLinkEl.setAttribute('href', url)
-        hostedLinkEl.innerText = `image is now hosted here`
-    }
-
-
-    submitBtn.addEventListener('click', (e) => {
-        e.preventDefault()
-        if (!myFile) return
-        const description = descriptionInputEl.value; // capture a value from form input
-        resetPage()
-
-        // create formData object & append all key: value pairs
+    const displaySelectedFileName = (name) => fileInputLabel.innerText = name
+    const collectFormData = (myFile) => {
         const formData = new FormData();
+        const description = descriptionInputEl.value; // capture a value from form input
         formData.append('myFile', myFile);
         formData.append('description', description);
-        formData.append('item1', 'another item from the form');
+        return formData
+    }
+    const validateForm = (file, password) => {
+        if (file && password) {
+            submitBtn.removeAttribute('disabled');
+            return true
+        }
+        submitBtn.setAttribute('disabled', true);
+        return false
+    }
+    const toggleLoading = () => submitBtn.classList.toggle('is-loading')
+    const loadResults = (type, url) => {
+        mediaType[type].setAttribute('src', url);
+        displayMediaContent(mediaType[type]);
+        hostedLinkEl.setAttribute('href', url);
+        hostedLinkEl.innerText = `${type} is now hosted here`;
+        type !== 'image' && mediaType[type].setAttribute('controls', null);
+    };
+    const displayMediaContent = (element) => element.classList.toggle("is-hidden");
+    const resetPage = () => {
+        fileInputEl.value = ''; // clear file input
+        passwordInputEl.value = '';
+        descriptionInputEl.value = ''; // reset displayed text 
+        submitBtn.setAttribute('disabled', true); // disable submit btn
+        hostedLinkEl.innerText = ''; // reset link text
+        displaySelectedFileName(''); // remove file name
+        resetMediaElements();
+    };
+    const resetMediaElements = () => {
+        Object.keys(mediaType).forEach(type => {
+            mediaType[type].src = ''
+            mediaType[type].removeAttribute('controls')
+        });
+    };
+
+    passwordInputEl.addEventListener('input', (e) => {
+        if (!validateForm(myFile, e.target.value)) return
+        console.log('active')
+    })
+
+    formEl.addEventListener('submit', (e) => {
+        e.preventDefault()
+        if (!validateForm(myFile, passwordInputEl.value)) return
+        toggleLoading()
+        const formData = collectFormData(myFile)
 
         fetch('/api/upload', {
             method: 'POST',
+            headers: {
+                'Authorization': `bearer ${passwordInputEl.value}`
+            },
             body: formData
         })
+            // resetPage()
             .then(res => res.json())
-            .then((data) => {
-                console.log(data)
-                displayResults(data.url)
+            .then((body) => {
+                console.log(body);
+                resetPage()
+                const { mimetype, url } = body;
+                loadResults(mimetype.split('/')[0], url);
+                toggleLoading();
             })
-            .catch(err => console.error(err))
+            .catch(err => {
+                console.error(err);
+                toggleLoading();
+            });
     })
 
-    fileInputEl.addEventListener('change', ({ target }) => {
-
+    fileInputEl.addEventListener('change', function ({ target }) {
         if (target.files && target.files[0]) {
-            myFile = target.files[0]
-            img.onload = () => URL.revokeObjectURL(img.src); // no longer needed, free memory
-            img.src = URL.createObjectURL(myFile); // set src to blob url
-            const reader = new FileReader();
-            reader.readAsDataURL(myFile);
-            reader.onloadend = () => {
-                submitBtn.removeAttribute('disabled')
-                // console.log(reader.result)
-            };
-        }
+            myFile = target.files[0];
+            console.log(myFile);
+            displaySelectedFileName(myFile.name);
+
+            const type = myFile.type.split('/')[0];
+            resetMediaElements();
+            mediaType[type].src = URL.createObjectURL(myFile); // set element src to blob url
+            mediaType[type].setAttribute('controls', null);
+            type === 'video' && mediaType[type].setAttribute('type', myFile.type);
+            displayMediaContent(mediaType[type]);
+        };
+
+        const reader = new FileReader();
+        reader.readAsDataURL(myFile);
+        reader.onloadend = () => validateForm(myFile, passwordInputEl.value)
     });
-
-
 });
